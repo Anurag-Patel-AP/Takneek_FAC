3de402701b9c7dfcecfe7f43eb975a864402c58e
Anurag-Patel-AP <apiitk766@gmail.com>
2024-08-18 23:13:21 +0530

    Sortino Done

--- Task1.py
+++ Task1.py
@@ -4,4 +4,8 @@ rf=0.3
 re=rt.mean()
 std=rt.std()
 sharpe=(re-rf)/std
-print(round(sharpe,2))
+print("Sharpe Ratio =",round(sharpe,2))
+rtn=pd.Series([-0.4,-0.23])
+stdn=rtn.std()
+sortino=(re-rf)/stdn
+print("Sortino Ratio =",round(sortino,2))
